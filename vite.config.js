a937977d1d77f96b15ec89427a6f9857f2bf22e0
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-04-04 14:30:20 +0100

    okkay

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/frontend_test/", // Ensure correct base path
+  base: "/", // Ensure correct base path
   plugins: [react()],
 });
